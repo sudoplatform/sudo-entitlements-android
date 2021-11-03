ed3b899a15fcb5181cec12743a688a8876bf8e9a
Sudo Platform Engineering <sudoplatform-engineering@anonyome.com>
2021-11-03 03:42:44 +0000

    Release 2.3.1

--- sudoentitlements/src/main/java/com/sudoplatform/sudoentitlements/DefaultSudoEntitlementsClient.kt
+++ sudoentitlements/src/main/java/com/sudoplatform/sudoentitlements/DefaultSudoEntitlementsClient.kt
@@ -70,6 +70,16 @@ internal class DefaultSudoEntitlementsClient(
         private const val ERROR_AMBIGUOUS_ENTITLEMENTS = "sudoplatform.entitlements.AmbiguousEntitlementsError"
     }
 
+    /**
+     * Checksum's for each file are generated and are used to create a checksum that is used when
+     * publishing to maven central. In order to retry a failed publish without needing to change any
+     * functionality, we need a way to generate a different checksum for the source code.  We can
+     * change the value of this property which will generate a different checksum for publishing
+     * and allow us to retry.  The value of `version` doesn't need to be kept up-to-date with the
+     * version of the code.
+     */
+    private val version: String = "2.3.1"
+
     private val appSyncClient: AWSAppSyncClient
 
     init {
